620f2b1ed321fe55b3b671333c2d5ba9e5ea9345
dungngoo <ngodung06vn@gmail.com>
2023-03-10 16:47:20 +0700

    Tạo giao diện Header và các thành phần con trong giao diện

---
+++ src/components/Layout/components/FormAccount/index.js
@@ -0,0 +1,44 @@
+import classNames from 'classnames/bind';
+import styles from './FormAccount.module.scss';
+
+const cx = classNames.bind(styles);
+
+function FormAccount() {
+    return (
+        <div className={cx('header__account-login')}>
+            <h3 className={cx('header__acount-title')}>ĐĂNG NHẬP TÀI KHOẢN</h3>
+            <div className={cx('header__acount-wrap-input')}>
+                <input
+                    type="number"
+                    placeholder="Số điện thoại"
+                    className={cx('header__acount-input', 'header__account-phonenumber')}
+                />
+                <input
+                    type="password"
+                    placeholder="Mật khẩu"
+                    className={cx('header__acount-input', 'header__account-password')}
+                />
+            </div>
+            <div className={cx('header__account-pilocy')}>
+                This site is protected by reCAPTCHA and the Google
+                <a href="/">Privacy Policy</a>
+                and
+                <a href="/">Terms of Service</a>
+                apply.
+            </div>
+            <div className={cx('header__account-wrap-button')}>Đăng nhập</div>
+            <div className={cx('header__account-forgot-password')}>
+                <div className={cx('header__wrap-new-account')}>
+                    <span className={cx('header__text', 'header__newaccount')}>Khách hàng mới?</span>
+                    <span className={cx('header__text', 'header__createaccount')}>Tạo tài khoản</span>
+                </div>
+                <div className={cx('header__wrap-forgot-account')}>
+                    <span className={cx('header__text', 'header__fotgotpw')}>Quên mật khẩu?</span>
+                    <span className={cx('header__text', 'header__findpw')}>Khôi phục mật khẩu</span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default FormAccount;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,20 +1,83 @@
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faSearch,
+    faUser,
+    faAngleDown,
+    faCartShopping,
+    faMedal,
+    faCar,
+    faCheck,
+} from '@fortawesome/free-solid-svg-icons';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 import logoRestaurant from '~/assets/imgs/logo-restaurant1.png';
+import FormAccount from '../FormAccount';
 const cx = classNames.bind(styles);
 
 function Header() {
+    const [showLoginForm, setShowLoginForm] = useState(false);
+
+    const handleClick = () => {
+        setShowLoginForm(!showLoginForm);
+    };
+
     return (
         <div className={cx('wrapper')}>
-            <div className={cx('header-logo')}>
-                <img src={logoRestaurant} />
+            <div className={cx('header__wrap-logo')}>
+                <img src={logoRestaurant} className={cx('header-logo')} />
             </div>
-            <div className="header-container">
-                <div className="header-search"></div>
-                <div className="header-criteria"></div>
+            <div className={cx('header-container')}>
+                <div className={cx('header-content')}>
+                    <div className={cx('header__wrap-input-search')}>
+                        <input
+                            type="text"
+                            placeholder="Cá sốt trứng muối, đậu hũ hải sản"
+                            className={cx('header__input-search')}
+                            onFocus="script"
+                        ></input>
+                        <div className={cx('header__wrap-icon-search')}>
+                            <FontAwesomeIcon icon={faSearch} className={cx('header__icon-search')} />
+                        </div>
+                    </div>
+                    <div className={cx('header__criteria')}>
+                        <div>
+                            <FontAwesomeIcon icon={faMedal} className={cx('header__criteria-logo')} />
+                            Cam kết chất lượng
+                        </div>
+                        <div>
+                            <FontAwesomeIcon icon={faCar} className={cx('header__criteria-logo')} />
+                            Giao nhanh 60 phút - Freeship dù chỉ 1 món
+                        </div>
+                        <div>
+                            <FontAwesomeIcon icon={faCheck} className={cx('header__criteria-logo')} />
+                            Lỗi là đổi
+                        </div>
+                    </div>
+                </div>
+
+                <div className={cx('header-login')}>
+                    <div className={cx('header__wrap-logo-login')}>
+                        <FontAwesomeIcon icon={faUser} className={cx('header__logo-login')} />
+                    </div>
+                    <div className={cx('header__account')} onClick={handleClick}>
+                        <span>Đăng nhập / Đăng ký</span>
+
+                        <div className={cx('header__myaccount')}>
+                            <span>Tài khoản của tôi</span>
+                            <FontAwesomeIcon icon={faAngleDown} className={cx('header__myaccount-icon')} />
+                        </div>
+                    </div>
+                    {/* Hiện account Form đăng nhập */}
+                    {showLoginForm && <FormAccount />}
+                </div>
+                <div className={cx('header__cart')}>
+                    <div className={cx('header__wrap-cart-logo')}>
+                        <FontAwesomeIcon icon={faCartShopping} className={cx('header__wrap-cart-logo')} />
+                    </div>
+                    <span>Giỏ hàng</span>
+                </div>
             </div>
-            <div className="header-login"></div>
-            <div className="header-cart"></div>
         </div>
     );
 }
