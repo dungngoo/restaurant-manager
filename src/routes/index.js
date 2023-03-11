58ea8e72bbee2da2d827fc0fde4a257022126313
dungngoo <ngodung06vn@gmail.com>
2023-03-11 13:13:34 +0700

    Tạo page: Orders - Tạo Layout HeaderFooter

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,16 @@
 // Layouts
 import { HeaderOnly } from '~/components/Layout';
+import HeaderFooterLayout from '~/components/Layout/HeaderFooterLayout';
 // Pages
 import Home from '~/pages/Home';
 import News from '~/pages/News';
+import Tables from '~/pages/Orders';
 
 // Không cần login
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/news', component: News, layout: HeaderOnly },
+    { path: '/orders', component: Tables, layout: HeaderFooterLayout },
 ];
 // Không vào được nếu chưa login
 const privateRoutes = [];
