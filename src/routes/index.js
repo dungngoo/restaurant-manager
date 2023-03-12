be31e06c034b98399f2200f5751a07644a5c036e
nth090801 <thanhhoai160518@gmail.com>
2023-03-12 13:20:03 +0700

    LayoutProduct

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,14 @@ import HeaderFooterLayout from '~/components/Layout/HeaderFooterLayout';
 import Home from '~/pages/Home';
 import News from '~/pages/News';
 import Tables from '~/pages/Orders';
+import Product from '~/components/Layout/components/Product';
 
 // Không cần login
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/news', component: News, layout: HeaderOnly },
     { path: '/orders', component: Tables, layout: HeaderFooterLayout },
+    { path: '/product', component: Product, layout: HeaderFooterLayout },
 ];
 // Không vào được nếu chưa login
 const privateRoutes = [];
