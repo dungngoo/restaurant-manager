46bc6cdf2982c9fa0603ee878042f9cd74cae4e4
dungngoo <ngodung06vn@gmail.com>
2023-03-17 22:25:51 +0700

    Chỉnh sửa trang giới thiệu và trang dịch vụ

--- src/pages/Introduce/index.js
+++ src/pages/Introduce/index.js
@@ -5,7 +5,52 @@ const cx = classNames.bind(styles);
 function Introduce() {
     return (
         <div className={cx('wrapper')}>
-            <h1>Trang Giới thiệu</h1>
+            <h2>Giới thiệu</h2>
+            <p>
+                LUXURY PALACE được biết đến như là một CUNG ĐIỆN tổ chức hội nghị – tiệc cưới đẳng cấp bậc nhất tại
+                thành phố Hồ Chí Minh. Tại đây, chúng tôi sẽ mang đến cho bạn sự trải nghiệm dịch vụ hoàn hảo và chuyên
+                nghiệp nhất. Với tổng diện tích sử dụng lên đến gần 20.000 m2, trong đó bao gồm 10 sảnh tiệc, mỗi sảnh
+                tiệc có sức chứa từ 200 đến 1.500 khách, cùng lúc có thể đón tiếp trên 4.000 khách. Đáp ứng mọi yêu cầu
+                của bạn về hình thức cũng như quy mô tổ chức. Bất kể là một hội thảo vài chục người hay một sự kiện hàng
+                ngàn người, chúng tôi cũng sẽ cam kết chuẩn bị chỉnh chu và tỉ mỉ nhất trong từng chi tiết.
+            </p>
+
+            <img
+                className={cx('aligncenter')}
+                src="https://luxurypalace.vn/wp-content/uploads/2022/03/COK_6273.jpg"
+                alt=""
+            />
+
+            <img
+                className={cx('aligncenter')}
+                src="https://luxurypalace.vn/wp-content/uploads/2022/03/COK_6273.jpg"
+                alt=""
+            />
+
+            <p>
+                LUXURY PALACE mang trên mình một lối kiến trúc hoàng gia sang trọng, mỗi góc tại CUNG ĐIỆN là một khu
+                vực check- in thần thánh nhất dành cho quan khách. CUNG ĐIỆN HOÀNG GIA cùng với sự đầu tư về trang thiết
+                bị, hệ thống âm thanh ánh sáng hiện đại và đội ngũ phục vụ chuyên nghiệp sẽ mang đến sự hài lòng bậc
+                nhất cho quan khách.
+            </p>
+
+            <img
+                className={cx('aligncenter')}
+                src="https://luxurypalace.vn/wp-content/uploads/2022/03/COK_6273.jpg"
+                alt=""
+            />
+
+            <img
+                className={cx('aligncenter')}
+                src="https://luxurypalace.vn/wp-content/uploads/2022/03/COK_6273.jpg"
+                alt=""
+            />
+
+            <p>
+                Luôn đặt mình vào vị trí của khách hàng, lắng nghe và thấu hiểu chính là nền tảng để LUXURY PALACE tạo
+                nên một chất lượng dịch vụ hoàn hảo và trọn vẹn nhất.
+            </p>
+            <p></p>
         </div>
     );
 }
--- src/pages/Service/index.js
+++ src/pages/Service/index.js
@@ -1,11 +1,120 @@
 import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
 import styles from './Service.module.scss';
 const cx = classNames.bind(styles);
 
 function Service() {
     return (
         <div className={cx('wrapper')}>
-            <h1>Trang dịch vụ</h1>
+            <div className={cx('header')}>
+                <h2 className={cx('title')}>Dịch vụ</h2>
+                <div className={cx('wrap-service')}>
+                    <nav className={cx('navbar')}>
+                        <Link className={cx('navbar-link')} to="./conference">
+                            <span className={cx('span-item')}>Hội nghị</span>
+                        </Link>
+                        <Link className={cx('navbar-link')} to="./wedding">
+                            <span className={cx('span-item')}>Tiệc cưới</span>
+                        </Link>
+                        <Link className={cx('navbar-link')} to="./partyoutside">
+                            <span className={cx('span-item')}> Tiệc outside</span>
+                        </Link>
+                        <Link className={cx('navbar-link')} to="./partyoutside">
+                            <span className={cx('span-item')}> Sự kiện</span>
+                        </Link>
+                    </nav>
+                </div>
+            </div>
+            <div className={cx('content')}>
+                <div className={cx('header-content')}>
+                    <p className={cx('title-content')}>
+                        LUXURY PALACE được ví như MỘT CUNG ĐIỆN tổ chức tiệc cưới vừa HIỆN ĐẠI, vừa SANG TRỌNG lại không
+                        kém phần LÃNG MẠNG. Mỗi sảnh tiệc có thiết kế riêng biệt, độc đáo và đầy ấn tượng hứa hẹn đem
+                        đến cho bạn sự lựa chọn hoàn hảo nhất. Và chúng tôi tin rằng, mỗi tiệc cưới của bạn là một câu
+                        chuyện tình yêu tuyệt đẹp với những nụ cười hạnh phúc, những khoảnh khắc ý nghĩa và cảm xúc ngập
+                        tràn.
+                    </p>
+                    <p className={cx('introduce-content')}>
+                        LUXURY PALACE – CUNG ĐIỆN HOÀNG GIA sẽ cùng bạn tạo nên những dấu ấn khó phai trong ngày trọng
+                        đại đẹp nhất đời mình.
+                    </p>
+                </div>
+                <div className={cx('wrapper-img-content')}>
+                    <div className={cx('img-top')}>
+                        <i className={cx('icon-top', 'fa-solid fa-chevron-left', 'transform-left-top')}></i>
+                        <img
+                            className={cx('img-service')}
+                            src="https://luxurypalace.vn/wp-content/uploads/2021/01/LUL04115-2.jpg"
+                            alt=""
+                        ></img>
+                        <i className={cx('icon-top', 'fa-solid fa-chevron-right', 'transform-right-top')}></i>
+                    </div>
+                    <i className={cx('icon-bottom', 'fa-solid fa-chevron-left', 'transform-left-bottom')}></i>
+
+                    <div className={cx('imgs')}>
+                        <img
+                            className={cx('img-item')}
+                            src="https://luxurypalace.vn/wp-content/uploads/2021/01/LUL04115-2.jpg"
+                            alt=""
+                        />
+                        <img
+                            className={cx('img-item')}
+                            src="https://luxurypalace.vn/wp-content/uploads/2021/01/LUL04115-2.jpg"
+                            alt=""
+                        />
+
+                        <img
+                            className={cx('img-item')}
+                            src="https://luxurypalace.vn/wp-content/uploads/2021/01/LUL04115-2.jpg"
+                            alt=""
+                        />
+                        <img
+                            className={cx('img-item')}
+                            src="https://luxurypalace.vn/wp-content/uploads/2021/01/LUL04115-2.jpg"
+                            alt=""
+                        />
+                        <img
+                            className={cx('img-item')}
+                            src="https://luxurypalace.vn/wp-content/uploads/2021/01/LUL04115-2.jpg"
+                            alt=""
+                        />
+                        <img
+                            className={cx('img-item')}
+                            src="https://luxurypalace.vn/wp-content/uploads/2021/01/LUL04115-2.jpg"
+                            alt=""
+                        />
+                        <img
+                            className={cx('img-item')}
+                            src="https://luxurypalace.vn/wp-content/uploads/2021/01/LUL04115-2.jpg"
+                            alt=""
+                        />
+                        <img
+                            className={cx('img-item')}
+                            src="https://luxurypalace.vn/wp-content/uploads/2021/01/LUL04115-2.jpg"
+                            alt=""
+                        />
+                        <img
+                            className={cx('img-item')}
+                            src="https://luxurypalace.vn/wp-content/uploads/2021/01/LUL04115-2.jpg"
+                            alt=""
+                        />
+                        <img
+                            className={cx('img-item')}
+                            src="https://luxurypalace.vn/wp-content/uploads/2021/01/LUL04115-2.jpg"
+                            alt=""
+                        />
+                        <img
+                            className={cx('img-item')}
+                            src="https://luxurypalace.vn/wp-content/uploads/2021/01/LUL04115-2.jpg"
+                            alt=""
+                        />
+                    </div>
+
+                    {/* <i
+                        className={cx('icon-bottom', 'fa-solid fa-chevron-right', 'transform-right-bottom', 'right')}
+                    ></i> */}
+                </div>
+            </div>
         </div>
     );
 }
