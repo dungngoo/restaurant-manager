574f33a06f5e0bff8c45ac9fc03f764d507fb1a6
nth090801 <thanhhoai160518@gmail.com>
2023-03-18 12:48:35 +0700

    RightNavBar

--- src/pages/Libary/index.js
+++ src/pages/Libary/index.js
@@ -1,11 +1,84 @@
 import classNames from 'classnames/bind';
+import { useState } from 'react';
 import styles from './Libary.module.scss';
 const cx = classNames.bind(styles);
 
 function Libary() {
     return (
-        <div className={cx('wrapper')}>
-            <h1>Trang Khuyến mãi</h1>
+        <div className={cx('container')}>
+            <h2 className={cx('titleContainer')}>Hình ảnh</h2>
+            <div className={cx('navBar')}>
+                <ul className={cx('nav')}>
+                    <li className={cx('navItem')}>
+                        <a className={cx('navLink')}>1. Món ăn</a>
+                    </li>
+                    <li className={cx('navItem')}>
+                        <a className={cx('navLink')}>2. Sự kiện cưới</a>
+                    </li>
+                    <li className={cx('navItem')}>
+                        <a className={cx('navLink')}>3. Sự kiện công ty</a>
+                    </li>
+                    <li className={cx('navItem')}>
+                        <a className={cx('navLink')}>4. Sự kiên cá nhân khác</a>
+                    </li>
+                </ul>
+            </div>
+            <div className={cx('wrapper')}>
+                <div className={cx('row')}>
+                    <div className={cx('col6')}>
+                        <a href="" className={cx('h-100')}>
+                            <div className={cx('card')}>
+                                <img
+                                    className={cx('img')}
+                                    src="https://luxurypalace.vn/wp-content/uploads/2019/12/IMG_1144-scaled.jpg"
+                                ></img>
+                                <div className={cx('cardOverlay')}>
+                                    <h5 className={cx('cardTitle')}>1. Món ăn</h5>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className={cx('col6')}>
+                        <a href="" className={cx('h-100')}>
+                            <div className={cx('card')}>
+                                <img
+                                    className={cx('img')}
+                                    src="	https://luxurypalace.vn/wp-content/uploads/2020/01/LUL02306.jpg"
+                                ></img>
+                                <div className={cx('cardOverlay')}>
+                                    <h5 className={cx('cardTitle')}>2. Sự kiện cưới</h5>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className={cx('col6')}>
+                        <a href="" className={cx('h-100')}>
+                            <div className={cx('card')}>
+                                <img
+                                    className={cx('img')}
+                                    src="https://luxurypalace.vn/wp-content/uploads/2019/12/8288fe19db363c686527.jpg"
+                                ></img>
+                                <div className={cx('cardOverlay')}>
+                                    <h5 className={cx('cardTitle')}>3. Sự kiện công ty</h5>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className={cx('col6')}>
+                        <a href="" className={cx('h-100')}>
+                            <div className={cx('card')}>
+                                <img
+                                    className={cx('img')}
+                                    src="https://luxurypalace.vn/wp-content/uploads/2021/12/6.jpg"
+                                ></img>
+                                <div className={cx('cardOverlay')}>
+                                    <h5 className={cx('cardTitle')}>4. Sự kiện cá nhân khác</h5>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/Order/index.js
+++ src/pages/Order/index.js
@@ -1,11 +1,65 @@
+import { type } from '@testing-library/user-event/dist/type';
 import classNames from 'classnames/bind';
 import styles from './Order.module.scss';
 const cx = classNames.bind(styles);
 
 function Order() {
     return (
-        <div className={cx('wrapper')}>
-            <h1>Form Đặt tiệc</h1>
+        <div className={cx('row')}>
+            <div className={cx('c-5')}>
+                <form className={cx('formBooking')}>
+                    <h1 className={cx('titleHeading')}>Đặt tiệc tại đây</h1>
+
+                    <h2 className={cx('titleForm')}>Liên hệ đặt tiệc</h2>
+                    <div className={cx('bodyForm')}>
+                        <input name={'inputName'} type={'text'} className={cx('input')} placeholder="Họ và tên"></input>
+                        <input
+                            name={'inputPhoneNumber'}
+                            type={'number'}
+                            className={cx('input')}
+                            placeholder="Điện thoại"
+                        ></input>
+                        <input
+                            name={'inputDate'}
+                            type={'datetime-local'}
+                            className={cx('input')}
+                            placeholder=""
+                        ></input>
+                        <input
+                            name={'inputTypeParty'}
+                            type={'text'}
+                            className={cx('input')}
+                            placeholder="Loại tiệc"
+                        ></input>
+                        <input
+                            name={'inputTypeLobby'}
+                            type={'text'}
+                            className={cx('input')}
+                            placeholder="Loại sảnh"
+                        ></input>
+                        <input
+                            name={'inputTableNumber'}
+                            type={'number'}
+                            className={cx('input')}
+                            placeholder="Số bàn"
+                        ></input>
+                        <input name={'inputFood'} type={'text'} className={cx('input')} placeholder="Món ăn"></input>
+                    </div>
+                    <div className={cx('wrapper')}>
+                        <button className={cx('btnBooking')}>Đặt tiệc</button>
+                    </div>
+
+                    <div className={cx('footerBooking')}>
+                        <h3 className={cx('hotLine')}>Hotline: 0328038817</h3>
+                    </div>
+                </form>
+            </div>
+            <div className={cx('c-7')}>
+                <img
+                    className={cx('img')}
+                    src="https://images2.thanhnien.vn/Uploaded/ttt/images/Content/tin-tuc/cap-nhat/2017_09_w4/luxury_palace/IMG_1778.jpg"
+                ></img>
+            </div>
         </div>
     );
 }
